92df4006e5abac93b331112f8560251640bc4d5f
RoshanJose7 <roshanstark007@gmail.com>
2020-10-29 15:07:17 +0530

    Fix: Firebase Config

--- pages/api/db/FirebaseConfig.js
+++ pages/api/db/FirebaseConfig.js
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-	apiKey: 'AIzaSyBBlA-OyZU5hMgIOichYAOPKskomxmwmi4',
+	apiKey: process.env.API_KEY,
 	authDomain: process.env.AUTH_DOMAIN,
 	databaseURL: process.env.DATABASE_URL,
-	projectId: 'mytodo-c0217',
+	projectId: process.env.PROJECT_ID,
 	storageBucket: process.env.STORAGE_BUCKET,
 	messagingSenderId: process.env.MESSAGING_SENDER_ID,
 	appId: process.env.APP_ID,
