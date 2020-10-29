d52786a449cb3cb2077b4097a9b0c6b93253472b
RoshanJose7 <roshanstark007@gmail.com>
2020-10-29 15:25:56 +0530

    Add Config

--- pages/api/db/FirebaseConfig.js
+++ pages/api/db/FirebaseConfig.js
@@ -1,10 +1,9 @@
-export const firebaseConfig = {
-	apiKey: process.env.API_KEY,
-	authDomain: process.env.AUTH_DOMAIN,
-	databaseURL: process.env.DATABASE_URL,
-	projectId: process.env.PROJECT_ID,
-	storageBucket: process.env.STORAGE_BUCKET,
-	messagingSenderId: process.env.MESSAGING_SENDER_ID,
-	appId: process.env.APP_ID,
-	measurementId: process.env.MEASUREMENT_ID
+const firebaseConfig = {
+	apiKey: 'AIzaSyBdenoXszJWJbhOfv-2E9C8EB_Lm4w4dT4',
+	authDomain: 'todoapp-development-86325.firebaseapp.com',
+	databaseURL: 'https://todoapp-development-86325.firebaseio.com',
+	projectId: 'todoapp-development-86325',
+	storageBucket: 'todoapp-development-86325.appspot.com',
+	messagingSenderId: '185058699900',
+	appId: '1:185058699900:web:85b6f3d34da6a8be1537f9'
 };
